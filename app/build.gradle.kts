c76e207bb7773251adcfa6a923cae7a825c25aaa
ngonguyen <ngonguyenvan910@gmail.com>
2024-01-17 19:39:28 +0700

    17/1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,23 +36,28 @@ android {
     buildFeatures {
         viewBinding = true
     }
+
+
 }
+
+
+
+
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+    implementation("com.android.support:support-annotations:28.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("io.grpc:grpc-okhttp:1.32.2")
-    implementation ("androidx.appcompat:appcompat:1.3.0")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel:2.4.1")
-    implementation ("androidx.lifecycle:lifecycle-livedata:2.4.1")
-
-
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
